d0ad3901a69b4d4a277698e32db87ed0c6766e91
Daniel Jimenez <danieljimenezch@Daniels-MacBook-Pro.local>
2023-06-10 16:55:36 -0600

    create function to add to cart

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListener();
+function cargarEventListener(){
+    //Cuando se agrega un curso presionando en el botón de agregar
+
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+//Funciones
+function agregarCurso(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')) {
+        console.log('Agregando al carrito...')
+    }
+}
